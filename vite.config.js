7ed164b8084259b7e150a3345570ede709a56140
ebungsoul <ebungsoul@gmail.com>
2025-07-02 00:52:37 +0900

    smart config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/everstone-shop-planner/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'serve' ? '/' : '/everstone-shop-planner/',
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+  }
 })
