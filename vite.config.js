e061adeaa6ad84d682d6b33ce50aa3815e6e6db3
ebungsoul <ebungsoul@gmail.com>
2025-07-02 00:22:27 +0900

    cleaning

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // 👇 이 부분을 추가하세요
-  base: '/everstone-shop-planner/', // '/<리포지토리명>/' 형식으로 입력합니다.
+  base: '/everstone-shop-planner/',
   plugins: [
     react(),
     tailwindcss(),
